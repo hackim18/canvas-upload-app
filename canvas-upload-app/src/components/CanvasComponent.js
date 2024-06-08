52c959bd514df8a33d1493936f941bc87b48f822
Hackim18 <khakim668@gmail.com>
2024-06-09 01:14:55 +0700

    feat: add canvas components

---
+++ canvas-upload-app/src/components/CanvasComponent.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from "react";
+
+const CanvasComponent = ({ imageURL, canvasSize, imageProps, setImageProps }) => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    // Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (imageURL) {
+      const img = new Image();
+      img.src = imageURL;
+      img.onload = () => {
+        const { x, y, width, height } = imageProps;
+        ctx.drawImage(img, x, y, width, height);
+      };
+    }
+  }, [imageURL, canvasSize, imageProps]);
+
+  return <canvas ref={canvasRef} width={canvasSize.width} height={canvasSize.height} style={{ border: "1px solid black" }} />;
+};
+
+export default CanvasComponent;
